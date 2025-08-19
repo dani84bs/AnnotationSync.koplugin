f3e147ce58ae6413e44034e0c800fb995abb320f
Daniele Trainini <dani84bs@gmail.com>
2025-08-19 18:36:12 +0200

    feat: Add submenus

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ local AnnotationSyncPlugin = WidgetContainer:extend{
 local filemanager_order = require("ui/elements/filemanager_menu_order")
 local reader_order = require("ui/elements/reader_menu_order")
 
-local utils = require("plugins/AnnotationSync.koplugin/utils")
+local utils = require("utils")
 utils.insert_after_statistics(filemanager_order, "annotation_sync_plugin")
 utils.insert_after_statistics(reader_order, "annotation_sync_plugin")
 
@@ -22,12 +22,23 @@ function AnnotationSyncPlugin:addToMainMenu(menu_items)
     menu_items.annotation_sync_plugin = {
         text = _("Annotation Sync"),
         sorting_hint = "tools",
-        callback = function()
-            UIManager:show(InfoMessage:new{
-                text = _("Sync annotations between devices."),
-                timeout = 2
-            })
-        end
+        sub_item_table = {{
+            text = _("Settings"),
+            callback = function()
+                UIManager:show(InfoMessage:new{
+                    text = _("Settings stub - not yet implemented."),
+                    timeout = 2
+                })
+            end
+        }, {
+            text = _("Manual Sync"),
+            callback = function()
+                UIManager:show(InfoMessage:new{
+                    text = _("Manual Sync stub - not yet implemented."),
+                    timeout = 2
+                })
+            end
+        }}
     }
 end
 
