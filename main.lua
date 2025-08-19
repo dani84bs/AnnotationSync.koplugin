766e657fa688ed0517d2c81f02c4c1aa02fb4984
Daniele Trainini <dani84bs@gmail.com>
2025-08-19 19:00:28 +0200

    feat: Let the user choose the sync directory

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local InfoMessage = require("ui/widget/infomessage")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
+local T = require("ffi/util").template
 
 local AnnotationSyncPlugin = WidgetContainer:extend{
     name = "AnnotationSync"
@@ -25,17 +26,39 @@ function AnnotationSyncPlugin:addToMainMenu(menu_items)
         sub_item_table = {{
             text = _("Settings"),
             callback = function()
-                UIManager:show(InfoMessage:new{
-                    text = _("Settings stub - not yet implemented."),
-                    timeout = 2
-                })
+                local plugin = self
+                local SyncService = require("apps/cloudstorage/syncservice")
+                local sync_service = SyncService:new{}
+                sync_service.onConfirm = function(server)
+                    -- Save the chosen cloud directory path to settings
+                    G_reader_settings:saveSetting("cloud_download_dir", server.url)
+                    UIManager:show(InfoMessage:new{
+                        text = T(_(
+                            "Cloud download directory set to:\n%1\nPlease restart KOReader for changes to take effect."),
+                            server.url),
+                        timeout = 4
+                    })
+                    UIManager:close()
+                    if plugin and plugin.ui and plugin.ui.menu and plugin.ui.menu.showMainMenu then
+                        plugin.ui.menu:showMainMenu()
+                    end
+                end
+                UIManager:show(sync_service)
             end
         }, {
             text = _("Manual Sync"),
+            enabled = (G_reader_settings:readSetting("cloud_download_dir") or "") ~= "",
             callback = function()
+                local dir = G_reader_settings:readSetting("cloud_download_dir")
+                local msg
+                if dir and dir ~= "" then
+                    msg = T(_("Current cloud directory:\n%1"), dir)
+                else
+                    msg = _("No cloud directory selected.")
+                end
                 UIManager:show(InfoMessage:new{
-                    text = _("Manual Sync stub - not yet implemented."),
-                    timeout = 2
+                    text = msg,
+                    timeout = 3
                 })
             end
         }}
