7bbea28efa8c4d86a26a759087be4c690ed20ddc
Daniele Trainini <dani84bs@gmail.com>
2025-11-16 15:14:01 +0100

    feat(meta): add version

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,6 @@ local _ = require("gettext")
 return {
     name = "AnnotationSync",
     fullname = _("Annotation Sync"),
-    description = _([[Sync annotations between devices.]])
+    description = _([[Sync annotations between devices.]]),
+    version = "v0.3.0"
 }
